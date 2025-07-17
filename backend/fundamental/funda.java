4f1ed6fde0303b8155d3c7377eaa17dfa4d88708
revanth0siddigari <revanthsiddigari@gmail.com>
2025-07-17 23:42:58 +0530

    17th

---
+++ backend/fundamental/funda.java
@@ -0,0 +1,9 @@
+package Backend.fundamental;
+
+public class funda {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        // This is a simple Java program that prints "Hello, World!" to the console.
+        // You can add more functionality here as needed.
+    }
+}
