0cc04c3be05eb2cdedd75e0cd50fc4ccbc8b78f4
revanth0siddigari <revanthsiddigari@gmail.com>
2025-07-18 23:56:07 +0530

    18-7

---
+++ backend/Appliction/Application.java
@@ -0,0 +1,44 @@
+package Backend.Appliction;
+
+import java.util.Date;
+
+public class Application {
+    public static void main(String[] args) {
+        String Last_Name = "Siddigari";
+        String First_Name = "Revanth";
+        String Middle_Name = "Reddy";
+        String Complete_Address = "gandhi nagar 1st line ";
+        String Date_Of_birth = "12/12/1998";
+        String Birth_Place = "hyderabad";
+        String Citizenship = "Indian";
+        String Civil_Status = "Single";
+        int Contact_Number = 1234567890;
+        String Religion = "Hindu";
+        int Contact_Number_Spouse = 1234567890;
+        String Father_Name = "Example1";
+        String Father_Occupation = "Example1";
+        int Father_Contact_number = 1234567890;
+        String Mother_Name = "Example1";
+        String Mother_Occupation = "Example1";
+        int Mother_Contact_number = 1234567890;
+        String Guardian_Name = "Example1";
+        String Guardian_Address = "Example1";
+        int Guardian_Contact_number = 1234567890;
+        String Senior_High_School = "exam";
+        String SHS_Strand = "example";
+        String Last_School_Attended = "school";
+        Short Year_Gratuated = 2000;
+        double Gen_Average = 76.6;
+        String Course = "Science";
+        short Last_School_Year = 1999;
+        char ALS_passer = 'Y'; 
+        char family_4p_Beneficiary = 'y';
+        char family_PWD = 'N';
+        short Year_Taken = 000;
+        double Average = 0.0;
+        int Ps_ID_No = 12345678;
+        String Type_of_Disability = "None";
+
+
+    }
+}
---
+++ backend/Appliction/Car_specs.java
@@ -0,0 +1,17 @@
+package Backend.Appliction;
+
+public class Car_specs {
+    public static void main(String[] args) {
+        String Engine_Type = "mHAWK 4 Cylinder";
+        String Displacement = "2184 cc";
+        String Max_Power = "130bhp@3750rpm";
+        String Max_Torque = "300Nm@1600-2800rpm";
+        byte No_of_Cylinders = 4;
+        byte Valves_Per_Cylinder = 4;
+        String Fuel_Supply_System = "CRDi";
+        Boolean Turbo_Charger = true;
+        String Transmission_Type = "Manual";
+        String Gearbox = "6-Speed";
+        String Drive_Type = "RWD";
+    }
+}
