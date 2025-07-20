c35e7cc450522e3029ee9eaf98f7dd9023fee46a
revanth0siddigari <revanthsiddigari@gmail.com>
2025-07-20 23:07:39 +0530

    upadting 18-7

--- backend/Appliction/Application.java
+++ backend/Appliction/Application.java
@@ -39,6 +39,48 @@ public class Application {
         int Ps_ID_No = 12345678;
         String Type_of_Disability = "None";
 
+        System.out.println("Personal Information:");
+        System.out.println("Full Name: " + First_Name + " " + Middle_Name + " " + Last_Name);
+        System.out.println("Address: " + Complete_Address);
+        System.out.println("Date of Birth: " + Date_Of_birth);
+        System.out.println("Place of Birth: " + Birth_Place);
+        System.out.println("Citizenship: " + Citizenship);
+        System.out.println("Civil Status: " + Civil_Status);
+        System.out.println("Contact Number: " + Contact_Number);
+        System.out.println("Religion: " + Religion);
+        System.out.println("Spouse Contact Number: " + Contact_Number_Spouse);
+        System.out.println();
+
+        System.out.println("Family Information:");
+        System.out.println("Father's Name: " + Father_Name);
+        System.out.println("Father's Occupation: " + Father_Occupation);
+        System.out.println("Father's Contact Number: " + Father_Contact_number);
+        System.out.println("Mother's Name: " + Mother_Name);
+        System.out.println("Mother's Occupation: " + Mother_Occupation);
+        System.out.println("Mother's Contact Number: " + Mother_Contact_number);
+        System.out.println("Guardian's Name: " + Guardian_Name);
+        System.out.println("Guardian's Address: " + Guardian_Address);
+        System.out.println("Guardian's Contact Number: " + Guardian_Contact_number);
+        System.out.println();
+
+        System.out.println("Education Information:");
+        System.out.println("Senior High School: " + Senior_High_School);
+        System.out.println("SHS Strand: " + SHS_Strand);
+        System.out.println("Last School Attended: " + Last_School_Attended);
+        System.out.println("Year Graduated: " + Year_Gratuated);
+        System.out.println("General Average: " + Gen_Average);
+        System.out.println("Course: " + Course);
+        System.out.println("Last School Year: " + Last_School_Year);
+        System.out.println();
+
+        System.out.println("Additional Information:");
+        System.out.println("ALS Passer: " + ALS_passer);
+        System.out.println("4Ps Beneficiary: " + family_4p_Beneficiary);
+        System.out.println("Family PWD: " + family_PWD);
+        System.out.println("Year ALS Taken: " + Year_Taken);
+        System.out.println("ALS Average: " + Average);
+        System.out.println("PSA ID No: " + Ps_ID_No);
+        System.out.println("Type of Disability: " + Type_of_Disability);
 
     }
 }
--- backend/Appliction/Car_specs.java
+++ backend/Appliction/Car_specs.java
@@ -13,5 +13,17 @@ public class Car_specs {
         String Transmission_Type = "Manual";
         String Gearbox = "6-Speed";
         String Drive_Type = "RWD";
+
+        System.out.println("Engine Type: " + Engine_Type);
+        System.out.println("Displacement: " + Displacement);
+        System.out.println("Max Power: " + Max_Power);
+        System.out.println("Max Torque: " + Max_Torque);
+        System.out.println("Number of Cylinders: " + No_of_Cylinders);
+        System.out.println("Valves Per Cylinder: " + Valves_Per_Cylinder);
+        System.out.println("Fuel Supply System: " + Fuel_Supply_System);
+        System.out.println("Turbo Charger: " + (Turbo_Charger ? "Yes" : "No"));
+        System.out.println("Transmission Type: " + Transmission_Type);
+        System.out.println("Gearbox: " + Gearbox);
+        System.out.println("Drive Type: " + Drive_Type);
     }
 }
