7357cccc8aaf35d59e71b93f950e3e4c4a9d89b9
revanth0siddigari <revanthsiddigari@gmail.com>
2025-07-31 23:55:01 +0530

    31-7

---
+++ backend/LMS5/trainer_payments.java
@@ -0,0 +1,24 @@
+package Backend.LMS5;
+
+import six_class_obj_met.Student;
+
+import java.util.Scanner;
+
+public class trainer_payments {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        student s1 = new student();
+        
+        s1.Student_info();
+
+        s1.final_achievement();
+
+        trainer t1 = new trainer();
+
+        t1.trainer_info();
+
+        t1.session_payment();
+    }
+}
